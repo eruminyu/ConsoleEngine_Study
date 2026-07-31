45d4fe73840d89a98267b88a1d452dbf612006a3
Serian <eruminyu@gmail.com>
2026-07-31 15:55:27 +0900

    Level&Actor
    레벨에서 액터를 관리하도록 업데이트

--- CraftEngine/Actor/Actor.h
+++ CraftEngine/Actor/Actor.h
@@ -1,7 +1,13 @@
 #pragma once
 
+
+#include <memory>		// std::weak_ptr 사용을 위해
+
 namespace Craft
 {
+	// 전방 선언
+	class Level;
+
 	// 가상 공간에 배치될 모든 액터의 기본 클래스
 	class Actor
 	{
@@ -25,6 +31,8 @@ namespace Craft
 		inline bool IsActive() const { return isActive && !hasExpired; }
 		inline bool HasExpired() const { return hasExpired; }
 
+		inline std::shared_ptr<Level> GetOwner() const { return owner.lock(); }
+		inline void SetOwner(std::weak_ptr<Level> newOwner) { owner = newOwner; }
 
 	protected:
 		// BeginPlay 이벤트 처리 여부 확인 플래그
@@ -35,6 +43,12 @@ namespace Craft
 
 		// 액터 삭제 요청 여부 확인 플래그
 		bool hasExpired = false;
+
+		// 오너십 - 이 액터를 소유하는 레벨 객체
+		// weak_ptr -> 약참조
+		// 실제 사용을 위해서는 해당 위치가 유효한지 확인해야함.
+	std::weak_ptr<Level> owner;
+
 	};
 
 
--- CraftEngine/Level/Level.cpp
+++ CraftEngine/Level/Level.cpp
@@ -1,4 +1,4 @@
-#include "Level.h"
+﻿#include "Level.h"
 
 namespace Craft
 {
@@ -17,14 +17,78 @@ namespace Craft
 	}
 	void Level::BeginPlay()
 	{
+		// 액터 초기화 시 1회 호출되는 이벤트
+		for (std::shared_ptr<Actor>& actor : actorList)
+		{
+			// 검증 - 이미 BeginPlay 처리된 경우에 건너뛰기
+			if (actor->HasBeganPlay())
+			{
+				continue;
+			}
 
+			// BeginPlay 이벤트 호출
+			actor->BeginPlay();
+		}
 	}
 	void Level::Tick(float deltaTime)
 	{
+		
+		for (std::shared_ptr<Actor>& actor : actorList)
+		{
+			// 검증 - 활성화 되지 않았으면 건나뛰기
+			if (actor->IsActive())
+			{
+				continue;
+			}
 
+		
+			actor->Tick(deltaTime);
+		}
 	}
 	void Level::Draw()
 	{
+		for (std::shared_ptr<Actor>& actor : actorList)
+		{
+			// 검증 - 활성화 되지 않았으면 건나뛰기
+			if (actor->IsActive())
+			{
+				continue;
+			}
+
+			// Draw 이벤트 호출
+			actor->Draw();
+		}
+	}
+	void Level::ProcessAddAndDestroyActors()
+	{
+		// 액터 제거 처리, 이터레이터 기반 루프
+		for (auto iterator = actorList.begin(); iterator != actorList.end();)
+		{
+			// 제거 요청된 액터인지 확인
+			auto actor = *iterator;
+			if (actor->HasExpired())
+			{
+				iterator = actorList.erase(iterator);
+				continue;
+			}
+			//다음 순번 처리를 위해 이터레이터(반복자, 포인터)의 증가처리
+			++iterator;
+		}
+
+		// 추가 처리
+		// 추가 요청된 목록이 없으면 종료.
+		if (addRequestedActorList.empty())
+		{
+			return;
+		}
+
+		for (const auto& Actor : addRequestedActorList)
+		{
+			actorList.emplace_back(Actor);
+		}
+
+		// 추가 처리된 목록을 정리.
+		addRequestedActorList.clear();
 
 	}
 }
