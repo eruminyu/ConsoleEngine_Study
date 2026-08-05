a231d20cad723866b344600ee69c20bebaa61524
Serian <eruminyu@gmail.com>
2026-08-05 15:38:18 +0900

    ScreenBuffer
    클래스 구현

---
+++ CraftEngine/Render/ScreenBuffer.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include <Math/Vector2.h>
+
+
+namespace Craft
+{
+	class ScreenBuffer
+	{
+	public:
+
+	private:
+		// 화면 버퍼 핸들
+		HANDLE buffer = nullptr;
+
+		// 화면 크기
+		Vector2 size;
+
+
+	};
+
+}
+
