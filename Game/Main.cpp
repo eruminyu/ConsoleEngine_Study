f680172d25b3d0acc2f1046f69b72b07b1997c3d
Serian <eruminyu@gmail.com>
2026-08-06 17:21:09 +0900

    sokoban 시작
    GameLevel 만들어서 불러오고, Map.txt오픈

--- Game/Main.cpp
+++ Game/Main.cpp
@@ -1,10 +1,12 @@
 ﻿#include <Engine/Engine.h>
-#include <Level/TestLevel.h>	
+//#include <Level/TestLevel.h>	
+#include <Level/GameLevel.h>
 
 
 int main() 
 {
 	Craft::Engine engine;
-	engine.AddNewLevel<TestLevel>();
+	//engine.AddNewLevel<TestLevel>();
+	engine.AddNewLevel<GameLevel>();
 	engine.Run();
 }
